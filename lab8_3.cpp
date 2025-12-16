835a8affc44fd75f8388ccb468bba536f9f049f5
G063Z <124440318+G063Z@users.noreply.github.com>
2025-12-16 16:21:18 +0700

    Update lab8_3.cpp

--- lab8_3.cpp
+++ lab8_3.cpp
@@ -11,3 +11,53 @@
 "Brook"
 "Jinbe"
 */
+#include<iostream>
+#include<iomanip>
+#include<string>
+using namespace std;
+
+int age, height;
+long long bounty;
+
+int main() {
+    
+    cout << "Enter your age: ";
+    cin >> age;
+    
+    if(age <= 25) {
+        cout << "Enter your height: ";
+        cin >> height;
+
+        if(height < 100) {
+            cout << "Your character = Chopper";
+        } 
+        else if(height < 180) {
+            cout << "Your character = Usopp";
+        } 
+        else {
+            cout << "Enter your bounty: ";
+            cin >> bounty;
+
+            if(bounty > 1100000000LL) {
+                cout << "Your character = Zoro";
+            } else {
+                cout << "Your character = Sanji";
+            }
+        }
+    } 
+    else if(age <= 60) {
+        cout << "Enter your bounty: ";
+        cin >> bounty;
+
+        if(bounty > 500000000LL) {
+            cout << "Your character = Jinbe";
+        } else {
+            cout << "Your character = Franky";
+        }
+    } 
+    else {
+        cout << "Your character = Brook";
+    }
+    
+    return 0;
+}
